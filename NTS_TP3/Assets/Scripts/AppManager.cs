5ff9c0586a82e34477538fbc698bdc2e23c30ee1
Ruben Maurizio <ruben.maurizio@epita.fr>
2023-03-30 14:07:25 +0200

    corentin

--- NTS_TP3/Assets/Scripts/AppManager.cs
+++ NTS_TP3/Assets/Scripts/AppManager.cs
@@ -7,7 +7,7 @@ public class AppManager : MonoBehaviour
     public GameObject enemyPrefab;
     public Transform cam;
     public int enemyNumber = 12;
-    public float spawnRange = 4f;
+    public float spawnRange = 8f;
     
     // Start is called before the first frame update
     void Start()
@@ -23,6 +23,7 @@ public class AppManager : MonoBehaviour
 
     public void SpawnEnemy()
     {
+       
         var x = cam.transform.position.x + Random.Range(-spawnRange, spawnRange);
         var y = cam.transform.position.y + Random.Range(-spawnRange, spawnRange);
         var z = cam.transform.position.z + Random.Range(-spawnRange, spawnRange);
