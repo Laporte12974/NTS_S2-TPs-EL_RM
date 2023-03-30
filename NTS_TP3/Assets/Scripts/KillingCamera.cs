a232bdbfeb6bf3ea93edc41b93e3ea34634f91d6
Laporte <theguywholivesinacomputer@gmail.com>
2023-03-30 15:52:05 +0200

    SoundEffects

--- NTS_TP3/Assets/Scripts/KillingCamera.cs
+++ NTS_TP3/Assets/Scripts/KillingCamera.cs
@@ -15,12 +15,18 @@ namespace TP3
         private Camera cam;
         public int _count = 0;
         public Text text;
+        private AudioClip[] _explosionSounds;
+        private AudioClip laserSound;
 
         // Start is called before the first frame update
         void Start()
         {
             _appManager = arorigin.GetComponent<AppManager>();
             cam = GetComponent<Camera>();
+            _explosionSounds[0] = Resources.Load<AudioClip>("SoundEffects/Chunky Explosion");
+            _explosionSounds[1] = Resources.Load<AudioClip>("SoundEffects/explosion");
+            _explosionSounds[2] = Resources.Load<AudioClip>("SoundEffects/synthetic_explosion_1");
+            laserSound = Resources.Load<AudioClip>("SoundEffects/laser5");
         }
 
         // Update is called once per frame
@@ -30,11 +36,13 @@ namespace TP3
             if (Input.touchCount <= 0) return;
             touchpos = Input.GetTouch(0).position;
             var ray = cam.ScreenPointToRay(touchpos);
+            
             if (Physics.Raycast(ray, out hit))
             {
                 var hitObj = hit.collider.gameObject;
                 if (hitObj.CompareTag("Enemy"))
                 {
+                    AudioSource.PlayClipAtPoint(_explosionSounds[Random.Range(0, _explosionSounds.Length)], hitObj.transform.position);
                     Destroy(hitObj);
                     _count++;
                     _appManager.SpawnEnemy();
