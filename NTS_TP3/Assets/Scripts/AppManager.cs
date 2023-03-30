467955fce5acd64eb8eedcdddaa960fbc175ea40
Ruben Maurizio <ruben.maurizio@epita.fr>
2023-03-30 12:54:27 +0200

    dae

---
+++ NTS_TP3/Assets/Scripts/AppManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform cam;
+    public int enemyNumber = 12;
+    public float spawnRange = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnEnemy()
+    {
+        var x = cam.transform.position.x + Random.Range(-spawnRange, spawnRange);
+        var y = cam.transform.position.y + Random.Range(-spawnRange, spawnRange);
+        var z = cam.transform.position.z + Random.Range(-spawnRange, spawnRange);
+        var spawnPos = new Vector3(x, y, z);
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+    }
+}
---
+++ NTS_TP3/Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Camera : MonoBehaviour
+{
+    
+
+}
